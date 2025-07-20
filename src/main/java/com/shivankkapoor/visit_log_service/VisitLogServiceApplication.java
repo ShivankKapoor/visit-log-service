7ffa601addc49874f4ff63e57df5ad99d0a721fd
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2025-07-20 15:34:16 -0500

    Initial commit

---
+++ src/main/java/com/shivankkapoor/visit_log_service/VisitLogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shivankkapoor.visit_log_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitLogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitLogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shivankkapoor/visit_log_service/VisitLogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivankkapoor.visit_log_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitLogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
