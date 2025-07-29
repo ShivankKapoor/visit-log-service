9ce47af8ab88c12527fa3062ef8aff0dfaf58af1
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2025-07-28 23:46:03 -0500

    Add days to uptime

--- src/main/java/com/shivankkapoor/visit_log_service/service/StatusService.java
+++ src/main/java/com/shivankkapoor/visit_log_service/service/StatusService.java
@@ -1,9 +1,7 @@
 package com.shivankkapoor.visit_log_service.service;
 
 import org.springframework.stereotype.Service;
-
 import reactor.core.publisher.Mono;
-
 import java.lang.management.ManagementFactory;
 import java.time.Duration;
 import java.util.HashMap;
@@ -11,22 +9,20 @@ import java.util.Map;
 
 @Service
 public class StatusService {
-
     public Mono<Map<String, Object>> getSystemUptimeReactive() {
         Map<String, Object> uptimeStatus = new HashMap<>();
         long uptimeMillis = ManagementFactory.getRuntimeMXBean().getUptime();
         Duration uptime = Duration.ofMillis(uptimeMillis);
-
         //uptimeStatus.put("uptimeMillis", uptimeMillis);
         uptimeStatus.put("uptimeFormatted", formatDuration(uptime));
-
         return Mono.just(uptimeStatus);
     }
-
+    
     private String formatDuration(Duration duration) {
-        long hours = duration.toHours();
+        long days = duration.toDays();
+        long hours = duration.toHoursPart();
         long minutes = duration.toMinutesPart();
         long seconds = duration.toSecondsPart();
-        return String.format("%dh %dm %ds", hours, minutes, seconds);
+        return String.format("%dd %dh %dm %ds", days, hours, minutes, seconds);
     }
 }
